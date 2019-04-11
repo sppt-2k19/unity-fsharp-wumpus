92a08b8e077dddca6258115062a7fecf43807fa1
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-04-11 14:25:37 +0200

    removed stuff

--- Assets/Editor/ReferenceContainer.cs
+++
@@ -1,6 +0,0 @@
-class ReferenceContainer
-{
-    public Reference UnityEngine { get; set; }
-    public Reference UnityEditor { get; set; }
-    public System.Collections.Generic.List<Reference> Additional { get; set; }
-}
